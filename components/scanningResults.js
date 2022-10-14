5b0e09cb9c22c936826c5d04d87844c2771be39b
Alton Ong <alton_ong@live.com.sg>
2022-10-14 11:45:10 +1100

    Added successful step transition for scanning QR code

--- components/scanningResults.js
+++ components/scanningResults.js
@@ -18,17 +18,30 @@ import React from "react";
 //
 // }
 
-const ResultsCard = ({result}) => {
-		// let filteredResults = filterResults(results);
-		return (
-			// This here would be the card component
-			<div className="w-4/6 h-90v flex items-center justify-center mx-auto">
-				<div className="Result-section border-2 border-sky-500 w-96 h-64">
-					<p>{result}</p>
+const ResultsCard = ({ result }) => {
+	// let filteredResults = filterResults(results);
+	// console.log(result);
+	// Convert the result from string to javascript object
+	// let resultObj = JSON.parse(result);
+	// console.log(resultObj);
+	console.log(result);
+	// Here I should read the results
+	return (
+		// This here would be the card component
+		<div className="w-5/6 h-90v flex items-center justify-center mx-auto">
+			<div className="Result-section flex flex-col items-center justify-center mx-auto border-4 border-amber-700 w-full h-96 text-center ">
+				<h1 className="text-3xl font-bold">Thank you for using</h1>
+				<div className="text-5xl">
+					recyc | <span className="font-bold">ABLE</span>
 				</div>
+				<h1 className="mt-14 text-4xl">
+					<span className="font-bold">Item: </span>
+					{result.item}
+				</h1>
+				parse
 			</div>
-		);
-
-}
+		</div>
+	);
+};
 
 export default ResultsCard;
--- pages/stepProgression/scanningStep.js
+++ pages/stepProgression/scanningStep.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect, Component } from "react";
 import Scanner from "../../components/scanner";
 import ResultContainerTable from "../../components/scanningResults";
-import styles from "../../styles/scanner.module.css"
+import styles from "../../styles/scanner.module.css";
 
 class ScanningStep extends Component {
 	// const [isScanning, setIsScanning] = useState(false);
@@ -13,7 +13,7 @@ class ScanningStep extends Component {
 			isScanning: true,
 			isLoading: false,
 			decodedResult: null
-		}
+		};
 
 		// This binding is necessary to make `this` work in the callback.
 		this.onNewScanResult = this.onNewScanResult.bind(this);
@@ -25,29 +25,34 @@ class ScanningStep extends Component {
 				<section className="flex flex-col justify-center content-center w-3/6 h-full mx-auto">
 					<div>
 						<div className={styles.scanner}>
-							{this.state.isScanning ? <Scanner
-								fps={10}
-								qrbox={250}
-								disableFlip={false}
-								qrCodeSuccessCallback={this.onNewScanResult}/> : null}
-
+							{this.state.isScanning ? (
+								<Scanner
+									fps={10}
+									qrbox={250}
+									disableFlip={false}
+									qrCodeSuccessCallback={this.onNewScanResult}
+								/>
+							) : (
+								<ResultContainerTable result={this.state.decodedResult} />
+							)}
 						</div>
 					</div>
 				</section>
-				<ResultContainerTable result={this.state.decodedResult} />
 			</>
 		);
 	}
 
 	onNewScanResult(decodedText, decodedResult) {
-		console.log(
-			"App [result]", decodedResult);
-		this.setState((state, props) => state.isScanning = false);
+		// console.log("App [result]", decodedResult);
+		this.setState((state, props) => (state.isScanning = false));
 		this.setState((state, props) => {
-			state.decodedResult = decodedText;
+			console.log(typeof decodedText);
+			console.log(decodedText);
+			const resultObj = JSON.parse(decodedText);
+			state.decodedResult = resultObj;
 			return state;
 		});
-
+		// return;
 	}
 }
 
