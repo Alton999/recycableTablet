897195a5da6dc50f88de66fdbe37ac5b0788beea
Alton <alton_ong@live.com.sg>
2022-11-05 12:56:43 +1100

    Added conditional loop animations for each bin required

--- components/scanningResults.js
+++ components/scanningResults.js
@@ -7,23 +7,55 @@ import styles from "../styles/loader.module.css";
 const ResultsCard = ({ result }) => {
 	console.log(result);
 
-	const [isLoading, setIsLoading] = useState(false);
+	const [animationType, setAnimationType] = useState(null);
+
+	const [isLoading, setIsLoading] = useState(true);
 
 	const reloadPage = () => {
 		window.location.reload();
 	};
 
+	const generateLoading = (binType) => {
+		console.log(binType);
+		if (binType === "Recycling") {
+			return (
+				<figure className={styles.loaderContainer}>
+					<div className={styles.recycling}></div>
+					<div></div>
+				</figure>
+			);
+		} else if (binType === "Paper") {
+			return (
+				<figure className={styles.loaderContainer}>
+					<div className={styles.paper}></div>
+					<div></div>
+				</figure>
+			);
+		} else {
+			return (
+				<figure className={styles.loaderContainer}>
+					<div className={styles.general}></div>
+					<div></div>
+				</figure>
+			);
+		}
+	};
+
 	useEffect(() => {
 		setIsLoading(true);
+
+		setAnimationType(result.bin);
+		console.log(animationType);
+
 		setTimeout(() => {
 			setIsLoading(false);
-		}, 5000);
+		}, 7000);
 	}, []);
 
 	// Here I should read the results
 	return (
 		// This here would be the card component
-		<div className="w-5/6 h-90v flex items-center justify-center mx-auto">
+		<div className="w-full h-90v flex items-center justify-center mx-auto">
 			<div className="Result-section flex flex-col items-center justify-center mx-auto border-4 border-amber-700 w-full text-center py-14">
 				<h1 className="text-3xl font-bold">Thank you for using</h1>
 				<div className="text-4xl mt-4">
@@ -36,23 +68,13 @@ const ResultsCard = ({ result }) => {
 							<span className="font-bold">Item: </span>
 							{result.item}
 						</h1>
-						<div className="mt-10">
-							<div className={styles.ldsDefault}>
-								<div></div>
-								<div></div>
-								<div></div>
-								<div></div>
-								<div></div>
-								<div></div>
-								<div></div>
-								<div></div>
-								<div></div>
-								<div></div>
-								<div></div>
-								<div></div>
+						<div className="">
+							<div className="flex justify-center mt-14">
+								{generateLoading(result.bin)}
 							</div>
-							<h2>
-								Please dispose of the {result.material} bottle in the
+
+							<h2 className="text-2xl font-bold mt-10">
+								Please dispose of the {result.material} bottle in the{" "}
 								{result.bin} bin.
 							</h2>
 						</div>
